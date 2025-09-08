6082d460cc4cdc9919c0bb481814ec6c3fbbd673
manny-uncharted <omoebun52@gmail.com>
2025-09-08 23:00:27 +0100

    fix issue to be compactible with the ci

--- scripts/install_p2pd.sh
+++ scripts/install_p2pd.sh
@@ -23,5 +23,20 @@ else
     sed -i '/toolchain/d' go.mod
 fi
 
-go get ./...
-go install ./...
+# Set GO111MODULE and use specific Go proxy settings for better compatibility
+export GO111MODULE=on
+export GOPROXY=https://proxy.golang.org,direct
+export GOSUMDB=sum.golang.org
+
+# Try to build with less strict dependency resolution
+go mod download || echo "Some dependencies failed to download, continuing..."
+go build -o p2pd-bin ./p2pd/
+go build -o p2p-keygen-bin ./p2p-keygen/
+
+# Install to GOPATH/bin
+GOPATH=${GOPATH:-$(go env GOPATH)}
+mkdir -p "$GOPATH/bin"
+cp p2pd-bin "$GOPATH/bin/p2pd"
+cp p2p-keygen-bin "$GOPATH/bin/p2p-keygen"
+
+echo "Successfully installed p2pd and p2p-keygen"
